ecaf88a586c004575a163320bf554f64620582c4
Mitchell Scott <10804314+rmitchellscott@users.noreply.github.com>
2021-05-24 16:22:03 -0500

    Adding back named-check action.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ log_info1() {
 log_info2() {
   echo "=====> $*"
 }
-
+cd zones/
 ZONESERIAL=$(date +"%s")
 if [ -z "$MAGICSTRING" ]; then
   MAGICSTRING="1 ; SERIALAUTOUPDATE"
@@ -71,7 +71,7 @@ else
   mkdir /root/.ssh
   echo -e $SSH_PRIVATE_KEY > /root/.ssh/id_rsa
   chmod 600 /root/.ssh/id_rsa
-  rsync $RSYNCPARAMS --rsync-path="sudo rsync" 'zones/' "$SSH_USER"@"$NS_HIDDENMASTER":"$RSYNC_DEST_DIR"
+  rsync $RSYNCPARAMS --rsync-path="sudo rsync" '.' "$SSH_USER"@"$NS_HIDDENMASTER":"$RSYNC_DEST_DIR"
   rc=$?; if [[ $rc != 0 ]]; then echo "rsync failed with $rc"; exit 1; fi
 fi
 
